0547d593769d28d467f85d6b0b48656076ffea4b
divleo <11596691+divleo@users.noreply.github.com>
2020-04-22 19:07:03 +0300

    Wrap App with BrowserRouter, update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App-container">
-      <HomePage />
+    <div className="app-container">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={() => 'HATS PAGE'} />
+      </Switch>
     </div>
   );
 }
